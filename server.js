3d43da8184e52b276f8daad9b3dd2a9003f35238
Igor Pravdin <pravdin_i_igor@ukr.net>
2022-05-27 20:04:45 +0300

    refactoring

--- server.js
+++ server.js
@@ -15,7 +15,17 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const {body, param, validationResult} = require('express-validator');
+
 require('express-async-errors');
+
+const validationWrapper = fn => (req, res, next) => {
+    const {errors} = validationResult(req);
+    if (errors.length > 0) {
+        return res.status(400).json({error: errors})
+    }
+    const result = fn(req, res, next);
+    return Promise.resolve(result).catch(next);
+};
 
 /*** MYSQL ***/
 
@@ -51,61 +61,44 @@ router.route('/lists/:limit?/:offset?')
     .get(
         param('limit').default(50).isInt({min: 1, max: 100}).toInt(),
         param('offset').default(0).isInt({min: 0}).toInt(),
-        async (req, res) => {
+        validationWrapper(async (req, res, next) => {
             const {params: {limit, offset}} = req;
-            const {errors} = validationResult(req);
-            if (errors.length > 0) {
-                res.status(400).json({error: errors});
-            } else {
-                const result = await query('select * from lists order by id desc limit ? offset ?;', [limit, offset]);
-                res.json(result);
-            }
-        });
+            const result = await query('select * from lists order by id desc limit ? offset ?;', [limit, offset]);
+            res.json(result);
+        }));
 
 // /list/:name
 
 router.route('/list/:name')
     .get(
         param('name').isUUID('4'),
-        async (req, res, next) => {
-            const {params: {name}} = req;
-            const errors = validationResult(req);
-            if (!errors.isEmpty()) {
-                res.status(400).json({error: errors.array()});
-            } else {
-                const [result] = await query('select * from lists where name = ?', [name]);
-                res.json(result);
-            }
-        })
+        validationWrapper(async ({params: {name}}, res, next) => {
+            const [result] = await query('select * from lists where name = ?', [name]);
+            res.json(result);
+        }))
     .post(
         param('name').isUUID('4'),
         body('status').isInt().toInt().isIn(Object.values(statuses)),
-        async (req, res, next) => {
-            const {params: {name}, body: {status}} = req;
-            const errors = validationResult(req);
-            if (!errors.isEmpty()) {
-                res.status(400).json({error: errors.array()});
-            } else {
-                const conn = await getConnection();
-                try {
-                    await promisify(conn.beginTransaction.bind(conn))();
-                    await promisify(conn.query.bind(conn))('update lists set status = ?, updated_at = now() where name = ?', [status, name]);
-                    const [result] = await promisify(conn.query.bind(conn))('select * from lists where name = ?', [name]);
-                    appEmitter.emit(LIST_STATUS_CHANGED, result);
-                    await promisify(conn.commit.bind(conn))();
-                    res.json(result);
-                } catch (err) {
-                    if (conn) {
-                        await promisify(conn.rollback.bind(conn))();
-                    }
-                    next(err);
-                } finally {
-                    if (conn) {
-                        conn.release();
-                    }
+        validationWrapper(async ({params: {name}, body: {status}}, res, next) => {
+            const conn = await getConnection();
+            try {
+                await promisify(conn.beginTransaction.bind(conn))();
+                await promisify(conn.query.bind(conn))('update lists set status = ?, updated_at = now() where name = ?', [status, name]);
+                const [result] = await promisify(conn.query.bind(conn))('select * from lists where name = ?', [name]);
+                appEmitter.emit(LIST_STATUS_CHANGED, result);
+                await promisify(conn.commit.bind(conn))();
+                res.json(result);
+            } catch (err) {
+                if (conn) {
+                    await promisify(conn.rollback.bind(conn))();
+                }
+                next(err);
+            } finally {
+                if (conn) {
+                    conn.release();
                 }
             }
-        });
+        }));
 
 app.use(router);
 
