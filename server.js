54d3c54186dcdea3684b43ccb968b306dfd75405
Igor Pravdin <pravdin_i_igor@ukr.net>
2022-05-27 11:05:25 +0300

    refactoring

--- server.js
+++ server.js
@@ -2,98 +2,105 @@
 
 /*** CONSTANTS ***/
 
-const statuses = require('./constants/status.js')
+const statuses = require('./constants/status.js');
 
 /*** CONFIG ***/
 
-const config = require('./config.js')
+const config = require('./config.js');
 
 /*** EXPRESS ***/
 
-const express = require('express')
-const app = express()
-const router = express.Router()
-const {body, param, validationResult} = require('express-validator')
+const express = require('express');
+const app = express();
+const router = express.Router();
+const {body, param, validationResult} = require('express-validator');
 
 /*** MYSQL ***/
 
-const mysql = require('mysql')
-const pool = mysql.createPool(config.mysql)
+const mysql = require('mysql');
+const pool = mysql.createPool(config.mysql);
+
+/*** EVENTS ***/
+
+const EventEmitter = require('events');
+
+class AppEmitter extends EventEmitter {
+}
+
+const appEmitter = new AppEmitter();
 
 /*** ROUTING ***/
 
-app.use(express.json())
+app.use(express.json());
 
 // /lists
 
-router
-    .route('/lists')
+router.route('/lists')
     .get((req, res) => {
         pool.query('select * from lists limit 100', (err, result) => {
-            if (err) throw err
-            res.json(result)
-        })
-    })
+            if (err) throw err;
+            res.json(result);
+        });
+    });
 
-// /list/:id
+// /list/:name
 
-router
-    .route('/list/:id')
-    .get(
-        param('id').isInt({min: 1}).toInt(),
+router.route('/list/:name')
+    .get( //get list info
+        param('name').isUUID('4'),
         (req, res) => {
-            const errors = validationResult(req)
+            const errors = validationResult(req);
             if (!errors.isEmpty()) {
-                return res.status(400).json({errors: errors.array()})
+                return res.status(400).json({error: errors.array()});
             }
-            const {params: {id}} = req;
-            pool.query('select * from lists where id = ?',
-                [id],
+            const {params: {name}} = req;
+            pool.query('select * from lists where name = ?',
+                [name],
                 (err, result) => {
-                    if (err) throw err
-                    res.json(result[0])
-                })
+                    if (err) throw err;
+                    res.json(result[0]);
+                });
         })
-    .post(
-        param('id').isInt({min: 1}).toInt(),
+    .post( //change list status
+        param('name').isUUID('4'),
         body('status').isInt().toInt().isIn(Object.values(statuses)),
         (req, res) => {
-            const errors = validationResult(req)
+            const errors = validationResult(req);
             if (!errors.isEmpty()) {
-                return res.status(400).json({errors: errors.array()})
+                return res.status(400).json({error: errors.array()});
             }
-            const {params: {id}, body: {status}} = req
-            pool.query(`update lists set status = ?, updated_at = now() where id = ?`,
-                [status, id],
+            const {params: {name}, body: {status}} = req;
+            pool.query('update lists set status = ?, updated_at = now() where name = ?',
+                [status, name],
                 (err, _) => {
-                    if (err) throw err
-                    res.json({status})
-                })
-        })
+                    if (err) throw err;
+                    res.json({status});
+                });
+        });
 
 // 404
 
-router.all('/*', (_, res) => res.status(404).json({error: 'Not found'}))
+router.all('/*', (_, res) => res.status(404).json({error: 'Not found'}));
 
-app.use(router)
+app.use(router);
 
 /*** SERVER ***/
 
 const server = app.listen(config.port, () => {
-    console.log(`APP SERVER started:`)
-    console.log(`> Port: ${config.port}`)
-})
+    console.log(`APP SERVER started:`);
+    console.log(`> Port: ${config.port}`);
+});
 
-process.on('SIGINT', () => shutDown())
-process.on('SIGTERM', () => shutDown())
+process.on('SIGINT', () => shutDown());
+process.on('SIGTERM', () => shutDown());
 
 function shutDown() {
-    console.log(`APP SERVER is closing:`)
+    console.log(`APP SERVER is closing:`);
     server.close(err => {
-        console.log(`> HTTP SERVER ... closed`)
+        console.log(`> HTTP SERVER ... closed`);
         pool.end(() => {
-            console.log('> DB CONNECTION ... closed')
-            process.exit(err ? 1 : 0)
-        })
-    })
+            console.log('> DB CONNECTION ... closed');
+            process.exit(err ? 1 : 0);
+        });
+    });
 }
