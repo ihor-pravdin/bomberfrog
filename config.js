e469aaf07de874e32fc43b27ad7d554a542069cf
Igor Pravdin <pravdin_i_igor@ukr.net>
2022-06-06 02:58:24 +0300

    utils/validator

--- config.js
+++ config.js
@@ -1,21 +1,29 @@
+'use strict';
+
+const os = require('node:os');
+
+const {check, conform} = require('./utils/validator');
+
+/*** EXPORTS ***/
+
 module.exports = {
 
     keeper: {
         workersDir: process.env.BF_WORKERS_DIR || './workers',
-        maxWorkers: process.env.BF_MAX_WORKERS || 8
+        maxWorkers: conform(check(process.env.BF_MAX_WORKERS || '').isInt().toInt()) || os.cpus().length
     },
 
     http: {
-        port: process.env.BF_API_SERVER_PORT || 3001,
+        port: conform(check(process.env.BF_API_SERVER_PORT || '').isInt().toInt()) || 3001,
         hostname: process.env.BF_API_HOSTNAME || '127.0.0.1'
     },
 
     mysql: {
-        connectionLimit: process.env.BF_MAX_POOL_SIZE || 10,
+        connectionLimit:conform(check(process.env.BF_MAX_POOL_SIZE || '').isInt().toInt()) || 10,
         host: process.env.BF_MYSQL_HOST || 'localhost',
         user: process.env.BF_MYSQL_USER || 'root',
         password: process.env.BF_MYSQL_PASSWORD || 'bomberfrog',
         database: process.env.BF_MYSQL_DATABASE || 'bomberfrog'
     }
 
-}
+};
---
+++ utils/validator.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const validator = require('validator');
+
+/*** CHAINS STATE STORAGE ***/
+
+const states = new WeakMap();
+
+/*** VALIDATOR CHAIN ***/
+
+function ValidatorChain(str) {
+    states.set(this, {
+        str,
+        errors: [],
+        sanitized: null
+    });
+}
+
+ValidatorChain.prototype.name = 'ValidatorChain';
+
+Object.keys(validator).forEach(fn => {
+    ValidatorChain.prototype[fn] = function (...args) {
+        const {str, errors, sanitized} = states.get(this);
+        const state = {str};
+        const result = validator[fn].call(validator, sanitized || str, ...args);
+        if (result === false) {
+            errors.push({
+                fn,
+                msg: `${fn}(${args.join(', ')}) failed with '${str}'`,
+                param: str
+            });
+        }
+        state.sanitized = typeof result !== 'boolean' ? result : sanitized;
+        state.errors = errors;
+        states.set(this, state);
+        return this;
+    };
+});
+
+/*** STATIC ***/
+
+ValidatorChain.check = str => new ValidatorChain(str);
+// ValidatorChain.check('10')
+
+ValidatorChain.isValid = chain => {
+    const {errors} = states.get(chain);
+    return !errors.length;
+};
+// ValidatorChain.isValid(ValidatorChain.check('10').isInt().toInt())
+
+ValidatorChain.conform = chain => {
+    const {errors, sanitized} = states.get(chain);
+    return !errors.length ? sanitized : null;
+};
+// ValidatorChain.conform(ValidatorChain.check('10').isInt().toInt())
+
+ValidatorChain.validationErrors = chain => states.get(chain).errors;
+// ValidatorChain.validationErrors(ValidatorChain.check('10').isInt().toInt())
+
+/*** EXPORTS ***/
+
+module.exports = {
+    validator,
+    check: ValidatorChain.check,
+    isValid: ValidatorChain.isValid,
+    conform: ValidatorChain.conform,
+    validationErrors: ValidatorChain.validationErrors
+};
